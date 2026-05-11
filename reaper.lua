0cf746cc6cc6b09c8f817c47fff28f5de49d9eb5
Swiftz007 <cxllme14zx@gmail.com>
2026-05-12 01:30:39 +0700

    Create reaper.lua

---
+++ reaper.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/lib1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/lib2.lua"))()
